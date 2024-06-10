5901fa82ee7ab051d0899aa9f28e35a0359d5b75
DerekRayna <987408090@qq.com>
2024-06-10 22:09:48 +0800

    基于antd二次开发

---
+++ src/PrimaryButton/demo/base.tsx
@@ -0,0 +1,11 @@
+// src/Button/demo/base.tsx
+
+import React from 'react';
+import { PrimaryButton } from 'derek-dumi';
+
+export default () => {
+
+  return (
+    <PrimaryButton>默认按钮</PrimaryButton>
+  );
+}
